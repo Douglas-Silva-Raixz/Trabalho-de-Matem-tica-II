96d8be6d67d57891b2e327f1d242629a296e5372
Douglas-Silva-Raixz <162138549+Douglas-Silva-Raixz@users.noreply.github.com>
2025-02-24 11:37:53 -0300

    l

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0.1;
+        tamanhoAtualFonte += 0.3;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 
     diminuiFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
+        tamanhoAtualFonte -= 0.3;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
